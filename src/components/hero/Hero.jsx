87b4c240cc0947c7d45dc440cc899aa7a1dfe4d4
Osama Nazakat <usamanazakat97@gmail.com>
2023-06-12 22:15:34 -0700

    hero section complete

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,37 @@
+import "./Hero.scss";
+import Wrapper from "../wrapper/Wrapper";
+import AppStore from "../../assets/app-store.svg";
+import PlayStore from "../../assets/play-store.svg";
+import HeroPhones from "../../assets/hero-phones.png";
+import PhonesBg from "../../assets/bg-hero-phones.svg";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <Wrapper>
+        <div className="hero__text">
+          <h1>
+            Stress Less <br /> Sleep Better!
+          </h1>
+          <p>Download now and you’ll be up and running in just minutes</p>
+
+          <div className="hero__btns">
+            <a href="#">
+              <img src={AppStore} alt="" />
+            </a>
+            <a href="#">
+              <img src={PlayStore} alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div className="hero__img-container">
+          <img src={HeroPhones} alt="" className="hero__phone-img" />
+          {/* <img src={PhonesBg} alt="" className="hero__phonebg-img" /> */}
+        </div>
+      </Wrapper>
+    </section>
+  );
+};
+
+export default Hero;
